8b989a74b9294859ba4985bd3a6bdba47f38cedb
Anastasia <shevhenko.nasta13@gmail.com>
2022-04-01 13:17:38 +0300

    working with server

--- dist/js/script.js
+++ dist/js/script.js
@@ -158,76 +158,170 @@ window.addEventListener ('DOMContentLoaded', () =>{
 
 // Используем классы для карточек
 
-class Menu {
-    constructor(src, alt, title, description, price, parentSelector, ... classes) {
-        this.src = src;
-        this.alt = alt;
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.parent = document.querySelector(parentSelector);
-        this.classes = classes;
-        this.transfer = 27;
-        this.changeToUAH(); 
-    }
-
-    changeToUAH() {
-        this.price = this.price * this.transfer; 
-    }
-
-    render() {
-        const element = document.createElement('div');
-
-        if (this.classes.length === 0) {
-            this.element = 'menu__item';
-            element.classList.add (this.element);
-        } else {
-            this.classes.forEach (className => element.classList.add(className));
+    class Menu {
+        constructor(src, alt, title, description, price, parentSelector, ... classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
+            this.transfer = 27;
+            this.changeToUAH(); 
         }
-        
 
-        element.innerHTML = `
-            <img src=${this.src} alt=${this.alt}>
-            <h3 class="menu__item-subtitle">${this.title}</h3>
-            <div class="menu__item-descr">${this.description}</div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-                <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-            </div>
-        `;
-        this.parent.append(element);
+        changeToUAH() {
+            this.price = this.price * this.transfer; 
+        }
+
+        render() {
+            const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add (this.element);
+            } else {
+                this.classes.forEach (className => element.classList.add(className));
+            }
+            
+
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.description}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
     }
-}
 
-new Menu(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container",
-    'menu__item'
-).render();
+    new Menu(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container",
+        'menu__item'
+    ).render();
 
-new Menu(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    14,
-    ".menu .container",
-    'menu__item'
-).render();
+    new Menu(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container",
+        'menu__item'
+    ).render();
 
-new Menu(
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    21,
-    ".menu .container",
-    'menu__item'
-).render();
+    new Menu(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container",
+        'menu__item'
+    ).render();
 
+    // Forms
+
+    /*
+    const forms = document.querySelectorAll ('form');
+
+    const message = {
+        loading: 'Loading',
+        success: 'Thanks. We will contact you!',
+        failure: 'Something got wrong'
+    };
+
+    forms.forEach( item => {
+        postData (item);
+    });
+
+    function  postData(form) {
+        form.addEventListener ('submit', (event) => {
+            event.preventDefault ();
+
+            let statusMessage = document.createElement ('div');
+            statusMessage.classList.add ('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+            
+            const request  = new XMLHttpRequest();
+            request.open ('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'multipart/form-data');
+            const formData =  new FormData(form);
+
+            request.send(formData);
+
+            request.addEventListener ('load', () => {
+                if (request.status === 200) {
+                    console.log (request.response);
+                    statusMessage.textContent = message.success;
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+
+        });
+    }
+    */
+
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы c вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+        
+            const request = new XMLHttpRequest();
+            request.open('POST', 'js/server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const obj = {};
+            formData.forEach(function (value, key) {
+                obj[key] = value;
+            });
+
+            const json = JSON.stringify (obj);       
+
+            request.send (json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout ( () => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
